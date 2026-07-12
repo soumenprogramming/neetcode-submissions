f8263962f176325dc6e69ee9943a477c346e852b
SOUMEN MANNA <103374779+soumenprogramming@users.noreply.github.com>
2026-07-12 15:30:03 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int stone : stones) {
+            list.add(stone);
+        }
+
+        while (list.size() > 1) {
+
+            Collections.sort(list);
+
+            int y = list.remove(list.size() - 1); 
+            int x = list.remove(list.size() - 1);
+
+            if (x != y) {
+                list.add(y - x);
+            }
+        }
+
+        return list.isEmpty() ? 0 : list.get(0);
+    }
+}
